e751d8c19426992eca4722d98fccfd9495b31301
and1teachera <angel.zl@abv.bg>
2019-05-05 14:48:43 +0300

    added i18n

--- pet-clinic-web/src/main/java/com/zlatenov/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/zlatenov/petclinic/controllers/OwnerController.java
@@ -21,7 +21,7 @@ public class OwnerController {
     }
 
     @RequestMapping({"","/","index", "index.html"})
-    public String listOwners(Model model) {
+    public String listOwners(final Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
 
