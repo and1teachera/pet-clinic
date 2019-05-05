e253f159d215f3355f4c132c527a81736f8eaae1
and1teachera <angel.zl@abv.bg>
2019-05-05 15:29:21 +0300

    added visits

--- pet-clinic-data/src/main/java/com/zlatenov/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/zlatenov/petclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package com.zlatenov.petclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Angel Zlatenov
  */
 
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(final Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/com/zlatenov/petclinic/model/Visit.java
@@ -0,0 +1,38 @@
+package com.zlatenov.petclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author Angel Zlatenov
+ */
+
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(final LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(final Pet pet) {
+        this.pet = pet;
+    }
+}
